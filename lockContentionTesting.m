cc520c300275915767f49c0d11085114457c1b6d
mattgou1d <mattgould44@gmail.com>
2018-10-06 11:42:01 -0700

    testing

--- lockContentionTesting.m
+++ lockContentionTesting.m
@@ -1,29 +1,34 @@
-method1 = zeros(1,10);
-method2 = zeros(50,1);
-method3 = zeros(50,1);
+method1 = zeros(1,100);
+method2 = zeros(1,100);
+method3 = zeros(1,100);
 
 % Run each method k times 
-for k = 1 : 10 
+for k = 1 : 100 
     [~,outM1] = system('./method1');
     method1(1,k) = str2double(outM1); 
 end 
 
-%{
-for k = 1 : 5 
+for k = 1 : 100 
     [~,outM2] = system('./method2');
-    method2(k,1) = str2double(outM2); 
+    method2(1,k) = str2double(outM2); 
 end 
 
-for k = 1 : 5 
+for k = 1 : 100
     [~,outM3] = system('./method3');
-    method3(k,1) = str2double(outM3); 
+    method3(1,k) = str2double(outM3); 
 end 
-%}
 
 % construct graphs 
-scatter(1:10,method1)
-xlabel('Run #');
-ylabel('Time milliseconds');
+figure
+h1 = scatter(method1,1:100,'b','o');
+hold on
+h2 = scatter(method2,1:100,'k','o');
+hold on
+h3 = scatter(method3,1:100,'g','o');
+legend([h1,h2,h3], 'Method 1', 'Method 2', 'Method 3')
+ylabel('Run #');
+xlabel('Time milliseconds');
+hold off 
 
 %{
 pd = makedist('Normal');
